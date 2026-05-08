d1a5e442fad05426ce909eee2e0a8110e53c29a5
Hans-Maulana <2472052@maranatha.ac.id>
2026-05-08 11:34:29 +0700

    landing page,route sign in out

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('landing-page');
+})->name('landing-page');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
